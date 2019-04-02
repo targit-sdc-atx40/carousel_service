bf98516f1c4c2f82a8a58f9eafa333d6800ed77b
Drew <dlockliear@gmail.com>
2019-04-02 14:02:47 -0500

    example for Sean  Please enter the commit message for your changes. Lines starting

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -1,24 +1,41 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import RelatedProductsCarousel from "./carousel.jsx";
+import axios from 'axios';
 
 class FormContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      items: null
     };
+  }
+
+  componentDidMount() {
+    axios.get('related-products')
+    .then(res => {
+      const relatedProducts = res.data;
+      this.setState({
+        items: relatedProducts
+      })
+      this.render()
+    })
+    .catch(console.error);
   }
   
   render() {
     
     return (
       <div>
-        <p>FormContainer</p>
+        <center>
+          <h2>Related Products</h2>
+          <RelatedProductsCarousel relatedProducts={this.state.items}/>
+        </center>
       </div>
     );
   }
 }
 export default FormContainer;
 
-const wrapper = document.getElementById("create-article-form");
+const wrapper = document.getElementById("root");
 wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
